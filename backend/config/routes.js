27f181427046f801646075c08d99a9314aa3a0ce
Henrique Martins <enrque.ns@gmail.com>
2018-10-15 08:45:33 -0300

    init

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,10 +1,19 @@
 module.exports = app => {
-    app.route('/users')
-        .post()
-        .get()
-    
-    app.route('/user')
-        .delete()
-        .update()
+    app.post('/singup', app.api.)
+    app.post('/singin', app.api.auth.singin)
+    app.post('/validateToken', app.api.auth.validateToken)
+
+    app.route('/empresas')
+        .get(app.api.empresa.get)
+        .post(app.api.empresa.insert)
+
+    app.route('/vagas')
+        .get(app.api.empresa.getVagas)
+
+    app.route('/vagas/:empresa_id')
+        .post(app.api.empresa)
+        .get(app.api.empresa.getVagasByEmpresaId)
+
+
 
 }
